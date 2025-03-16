d257bbc5131cde1bdaedb2f5b3dd52c1f6810c07
vit-posmourny <dr1v3r@email.cz>
2025-03-16 01:58:12 +0100

    added Chirp listener

--- app/Events/ChirpCreated.php
+++ app/Events/ChirpCreated.php
@@ -2,13 +2,14 @@
 
 namespace App\Events;
 
+use App\Models\Chirp;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class ChirpCreated
 {
@@ -17,7 +18,7 @@ class ChirpCreated
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(public Chirp $chirp)
     {
         //
     }
