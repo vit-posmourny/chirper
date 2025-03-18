5a65d753dbdb545a44e02726c8cdfc9eb8fb4d2a
vit-posmourny <dr1v3r@email.cz>
2025-03-18 03:19:29 +0100

    added EventServiceProvider

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -2,11 +2,13 @@
 
 namespace App\Listeners;
 
+use App\Models\User;
 use App\Events\ChirpCreated;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Notifications\NewChirp;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendChirpCreatedNotifications
+class SendChirpCreatedNotifications implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -21,6 +23,9 @@ class SendChirpCreatedNotifications
      */
     public function handle(ChirpCreated $event): void
     {
-        //
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+
+            $user->notify(new NewChirp($event->chirp));
+        }
     }
 }
---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use App\Events\ChirpCreated;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\ServiceProvider;
+use App\Listeners\SendChirpCreatedNotifications;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+
+
+class EventServiceProvider extends ServiceProvider
+{
+
+    protected $listen = [
+
+        ChirpCreated::class => [
+
+            SendChirpCreatedNotifications::class,
+        ],
+        Registered::class => [
+
+            SendEmailVerificationNotification::class
+        ],
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
