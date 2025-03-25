b0f3f4fd7fd817dd94e660155c20a2af782d5246
vit-posmourny <dr1v3r@email.cz>
2025-03-25 19:12:13 +0100

    installed alpine via npm install alpinejs

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,7 +6,5 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
